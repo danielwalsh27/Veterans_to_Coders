44e2301368872a284b66c74c29e4ca3248341fc5
Daniel Walsh <dewalsh27@yahoo.com>
2020-06-29 21:29:20 -0700

    Create 1_0_Hello_World.cpp

---
+++ 1_0_Hello_World.cpp
@@ -0,0 +1,14 @@
+// include the input / output library
+#include <iostream>
+using namespace std;
+
+// int main is used to being the program
+int main(){
+
+	// print hello world
+	cout << "Hello World" << endl;
+	cout << "USMC > US Army" << endl;
+
+	// returning the value of 0 to the end of the program
+	return 0;
+}
