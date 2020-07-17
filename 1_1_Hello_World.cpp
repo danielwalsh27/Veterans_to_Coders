832b36d81ebe3ecd6baad665e57bfdf660ea6cb3
Daniel Walsh <dewalsh27@yahoo.com>
2020-07-16 17:16:28 -0700

    Update and rename 1_0_Hello_World.cpp to 1_1_Hello_World.cpp

---
+++ 1_1_Hello_World.cpp
@@ -0,0 +1,17 @@
+// include the input / output library
+#include <iostream>
+
+//add namespace std
+using namespace std;
+
+// int main is used to being the program
+int main(){
+	//print hello world
+	cout << "Hello World" << endl;
+
+	//print Marine Corps is better than the Army, and all other branches
+	cout << "The Marines is much better than your branch!!!!!" << endl;
+
+	//returning the value of 0 to the end of the program
+	return 0;
+}
