d6bde402948028e76370bf1fe792c167c66b17cb
Daniel Walsh <dewalsh27@yahoo.com>
2020-07-16 21:56:47 -0700

    Update 1_2_Primitive_Data_Types.cpp

--- 1_2_Primitive_Data_Types.cpp
+++ 1_2_Primitive_Data_Types.cpp
@@ -1,32 +1,32 @@
-// include the input / output library
+//include the input /output library
 #include <iostream>
 
 //add namespace std
 using namespace std;
 
-// int main is used to being the program
+//int main is used to begin the program
 int main(){
-	//create 5 variables without assigning them
+	//create 5 variables
 	string branch;
 	int number;
 	double pi;
 	char yes;
-	bool onoff;
+	bool onOff;
 
-	//print out all the items
-	cout << branch << " "<< number << " " << pi << " " << yes << " " << onoff << endl;
+	//print out all 5 vars
+	cout << branch << " " << number << " " << pi << " " << yes << " " << onOff << endl;
 
 	//give all the types a value
 	branch = "USMC";
 	number = 620;
 	pi = 3.14159;
 	yes = 'y';
-	onoff = true;
+	onOff = true;
 
-	//print out the value of each type
-	cout << branch << " "<< number << " " << pi << " " << yes << " " << onoff << endl;
+	//print out the value of each
+	cout << branch << " " << number << " " << pi << " " << yes << " " << onOff << endl;
 
-	//create new variables based off existing variables
+	//create new variables based on existing variables
 	string laughableBranch = "Navy";
 	string laughablebranch = "Chair Force";
 	int newNumber = number + 100;
@@ -35,10 +35,9 @@ int main(){
 	//print out all new inputs
 	cout << "Is the " << laughableBranch << " a legit branch?" << endl;
 	cout << "Is the " << laughablebranch << " a legit branch?" << endl;
-	cout << "New number:" << newNumber << endl;
-	cout << "Pi divided by is: " << notPi << endl;
+	cout << "New Number: " << newNumber << endl;
+	cout << "Pi divided by 2 is: " << notPi << endl;
 
-
-	//returning the value of 0 to the end of the program
+	//return the value of 0 to end the program
 	return 0;
 }
