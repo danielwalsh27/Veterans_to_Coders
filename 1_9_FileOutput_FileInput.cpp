dbc83430b286c3cb1b8441545d2d5186cfde1d3b
Daniel Walsh <dewalsh27@yahoo.com>
2020-07-19 20:22:37 -0700

    Update 1_9_FileOutput_FileInput.cpp

--- 1_9_FileOutput_FileInput.cpp
+++ 1_9_FileOutput_FileInput.cpp
@@ -1,66 +1,40 @@
 //include the input output library
 #include <iostream>
+#include <fstream>
 using namespace std;
-
-//pass by pointer and pass by reference
-int add(int firstVar, int *varPoint){
-	cout << "\nNow we have passed the variable and pointer through the add function" << endl;
-	cout << "Dereferencing the pointer "<<firstVar<<" + "<<*varPoint<<" = "<< firstVar+*varPoint<<endl;
-	return firstVar + *varPoint;
-}
-int mult(int firstVar, int *varPoint){
-	cout << "\nNow we have passed the variable and pointer through the add function" << endl;
-	cout << "Dereferencing the pointer "<<firstVar<<" * "<<*varPoint<<" = "<< firstVar**varPoint<<endl;
-	return firstVar * *varPoint;
-}
-void RefAddOne(int &value){
-	cout << "We passed by reference to the RefAddOne function." << endl;
-	value = value + 1;
-}
-void RefTimesFive(int &value){
-	cout << "We passed by reference to the RefTimesFive function." << endl;
-	value = value * 5;
-}
-void RefSquare(int &value){
-	cout << "We passed by reference to the RefSquare function." << endl;
-	value = value * value;
-}
 
 //int main is used to begin the program
 int main(){
-	//create two variables, one int and an int pointer
-	int var1;
-	int *point;
+	//output stream - used to write to file
+	ofstream fout;
+	fout.open("ifThisFileDoesntExistItWillCreateIt.txt");
+	fout << "This is the first line in the txt file" << endl;
+	fout << "This is the second line" << endl;
+	fout << "third line" << endl;
 
-	cout << "Enter an integer: ";
-	cin >> var1;
+	//when we are done writing to the file we must close the file
+	fout.close();
 
-	//this is not a traditional assignment, your telling the pointer where to point to (address of var1)
-	point = &var1;
+	//create an array to store the items we read from the file
+	string linesInFile[10];
 
-	//this will display the memory address of the pointer
-	cout << point << endl;
-	cout << &var1 << endl;
+	//input stream - used to read from file
+	ifstream fin;
+	fin.open("ifThisFileDoesntExistItWillCreateIt.txt");
+	int i = 0;
 
-	//by adding the * in front of the pointer we are dereferencing the pointer, displaying var1
-	cout << *point<< endl;
-	cout << var1 << endl; //&var1 for memory address
+	//read items from file and store into an array
+	while(!fin.eof()){
+		getline(fin, linesInFile[i++]);
+		i++;
+	}
+	//when we are done writing to the file we must close the file
+	fin.close();
 
-	//function call to the pointer functions we created
-	cout << add(var1,point) << endl; //cannot be passed by * (deref of) or & (address of)
-	cout << mult(var1,point) << endl;
-
-	cout << "\nNumber BEFORE the reference calls: " << var1 << endl;
-
-	//function calls to the reference functions we created
-	RefAddOne(var1);
-	cout << "Now the int is " << var1 << "\n" << endl;
-	RefTimesFive(var1);
-	cout << "Now the int is " << var1 << "\n" << endl;
-	RefSquare(var1);
-	cout << "Now the int is " << var1 << "\n" << endl;
-
-	cout << "Number AFTER the reference calls: " << var1 << endl;
+	//print out all the contents of the array
+	for(int i = 0; i < 10; i++){
+		cout << i << ": " << linesInFile[i] << endl;
+	}
 
 	//return the value of 0 to end the program
 	return 0;
